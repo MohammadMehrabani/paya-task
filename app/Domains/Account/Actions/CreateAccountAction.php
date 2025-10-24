5bd0fb1d30ecf1543adde7d5ac3669a762b1aadc
Mohammd Mehrabani <mehrabani.developer@gmail.com>
2025-10-24 22:53:45 +0330

    remove unused files

--- app/Domains/Account/Actions/CreateAccountAction.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Domains\Account\Actions;
-
-class CreateAccountAction
-{
-
-}
--- app/Domains/Account/Actions/Transaction/CreateAccountTransactionAction.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Domains\Account\Actions\Transaction;
-
-class CreateAccountTransactionAction
-{
-
-}
